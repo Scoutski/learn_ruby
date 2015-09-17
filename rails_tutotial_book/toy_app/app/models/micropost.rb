55646307c343f4fedb3c27bbc0027c8c2248975b
mcooper <mikeyjc@gmail.com>
2015-09-17 11:56:40 +1000

    completed the toy_app in rails_tutorial_book

--- rails_tutotial_book/toy_app/app/models/micropost.rb
+++ rails_tutotial_book/toy_app/app/models/micropost.rb
@@ -1,2 +1,5 @@
 class Micropost < ActiveRecord::Base
+  belongs_to :user
+  validates :content, length: { maximum: 140 }
+  # only allows a micropost to be created if it as least 140 characters.
 end
